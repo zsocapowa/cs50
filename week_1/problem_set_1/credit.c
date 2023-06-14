c541cb33f4ce75e801d8460be7de3ab1d29c6265
zsoca <zsoca.powa@gmail.com>
2023-06-14 17:42:23 +0200

    work on credit problem

--- week_1/problem_set_1/credit.c
+++ week_1/problem_set_1/credit.c
@@ -1,27 +1,75 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+int get_long_lenght(long n)
+{
+    int count = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        count ++;
+    }
+    return count;
+}
+
+const char* card_type() {
+  return "Flavio";
+}
+
+
 
 int main(void) 
 {
     long n;
-    // long card = 4003600000000014;
-    n = get_long("card?");
-    printf("%li", n);
+    n = get_long("Number: ");
 
-// long int num = 1234567890;
-// int i;
+    int num_digits;
+    num_digits = get_long_lenght(n);
+    printf("%i\n", num_digits);
 
-// for (i = 0; i < sizeof(long int); i++) {
-//     printf("%ld ", num & 0xff);
-//     num >>= 8;
-// }
+    if (num_digits > 16)
+    {
+        printf("INVALID\n");
+    }
 
+    char str[num_digits];
+    sprintf(str, "%ld", n);
+
+
+    int first_sum = 0;
+    int second_sum = 0;
+    int iteration_num = 0;
+
+    for (int i = num_digits -1; i >= 0; i --)
+    {
+        printf("%c\n", str[i]);
+        int digit = str[i] - '0';
+        if (iteration_num % 2 == 0) {
+            second_sum += digit;
+        }
+        else {
+            int target_num = digit * 2;
+        
+            int first_digit = target_num / 10;
+            int second_digit = target_num % 10;
+
+            first_sum += first_digit + second_digit;
+        }
+    
+        iteration_num++;
+    }
+
+    int final_sum = first_sum + second_sum;
+    printf("%i\n", final_sum);
+
+    if (final_sum % 2 == 0){
+        printf("VALID\n");
+    }
+    else {
+        printf("INVALID\n");
+    }
 }
-
-// card number is stored in a database - when used, the creditor will now whom to bill
-// AM: 34, 37 / MasterCard: 51, 52, 53, 54, 55 / Visa: 4
-// checksum - detect typos without db query 
-// Luhn's Algorithm - determine if a credit card number is (syntactically) valid as follows:
 
 
 // cc -o credit credit.c -Wall -Werror -std=c11 -lcs50
