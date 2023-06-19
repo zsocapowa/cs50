3d147d50f3254adbaaf2b19abf196b7c3c4aa712
zsoca <zsoca.powa@gmail.com>
2023-06-19 17:03:01 +0200

    little tweaks

--- week_1/problem_set_1/credit.c
+++ week_1/problem_set_1/credit.c
@@ -29,7 +29,7 @@ int main(void)
     num_digits = get_long_lenght(n);                                       
     // printf("%i\n", num_digits);
 
-    if (num_digits > 16)
+    if (num_digits != 13 && num_digits != 15 && num_digits != 16)
     {
         printf("INVALID\n");
         return 0;
@@ -69,22 +69,17 @@ int main(void)
 
     if (final_sum % 10 == 0)
     {
-        if (num_digits == 15 && str[0] == '3' && (str[1] == '4' || str[1] == '7'))
+        if (str[0] == '3' && (str[1] == '4' || str[1] == '7'))
         {
             printf("AMEX\n");
             return 0;
         }
-        else if (num_digits == 13 && str[0] == '4')
+        else if (str[0] == '4')
         {
             printf("VISA\n");
             return 0;
         }
-        else if (num_digits == 16 && str[0] == '4')
-        {
-            printf("VISA\n");
-            return 0;
-        }
-        else if (num_digits == 16 && str[0] == '5' && (str[1] == '1' || str[1] == '2' || str[1] == '3' || str[1] == '4' || str[1] == '5'))
+        else if (str[0] == '5' && (str[1] == '1' || str[1] == '2' || str[1] == '3' || str[1] == '4' || str[1] == '5'))
         {
             printf("MASTERCARD\n");
             return 0;
