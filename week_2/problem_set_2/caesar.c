fb33f749d8102d6879e32bec759d8514d6a068f4
zsocapowa <zsoca.powa@gmail.com>
2023-07-22 22:13:39 +0200

    caesar  working version, needs  refactor though

--- week_2/problem_set_2/caesar.c
+++ week_2/problem_set_2/caesar.c
@@ -17,7 +17,7 @@ int main(int argc, string argv[])
 
     int n = 0;
     int size = strlen(argv[1]);
-    for (int i = size -1; i >= 0; i--)
+    for (int i = size - 1; i >= 0; i--)
     {
 
         if (isdigit(argv[1][i]) == 0)
@@ -40,8 +40,37 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    printf("%i\n", key);
-}
+    string usr_input = get_string("plaintext: ");
+    printf("ciphertext:  ");
 
+    int input_size = strlen(usr_input);
+
+    for (int i = 0; i < input_size; i++)
+    {
+        int start;
+        int num_to_add;
+        int ascii_value = usr_input[i];
+
+        if (ascii_value >= 65 && ascii_value <= 90)
+        {
+            start = 65;
+            num_to_add = (ascii_value - start + key) % 26;
+        }
+        else if (ascii_value >= 97 && ascii_value <= 122)
+        {
+            start = 97;
+            num_to_add = (ascii_value - start + key) % 26;
+        }
+        else
+        {
+            start = ascii_value;
+            num_to_add = 0;
+        }
+
+        int cipher_character = start + num_to_add;
+        printf("%c", cipher_character);
+    }
+    printf("\n");
+}
 
 // gcc -o caesar caesar.c -lcs50 -lm
