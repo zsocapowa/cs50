eef954d2af4165e404abbb9e2ddba79af64c861e
zsocapowa <zsoca.powa@gmail.com>
2024-03-08 08:35:45 +0100

    continue week_8

---
+++ week_8/problem_set_8/public/js/main.js
@@ -0,0 +1,66 @@
+// create document click that watches the nav links only
+document.addEventListener("click", (e) => {
+	const { target } = e;
+	if (!target.matches("#navbarSupportedContent a")) {
+		return;
+	}
+	console.log("eeeeeeb", e)
+	e.preventDefault();
+	urlRoute();
+});
+
+const urlRoutes = {
+	404: {
+		template: "/templates/404.html",
+		title: "404",
+	},
+	"/": {
+		template: "/templates/home.html",
+		title: "home",
+	},
+	"/pmtsz": {
+		template: "/templates/pmtsz.html",
+		title: "pmtsz",
+	},
+	"/contact": {
+		template: "/templates/contact.html",
+		title: "contact",
+	},
+};
+
+const urlRoute = (event) => {
+	event = event || window.event; // get window.event if event argument not provided
+	event.preventDefault();
+	// window.history.pushState(state, unused, target link);
+	console.log("reeef", event.target.href)
+	window.history.pushState({}, "", event.target.href);
+	console.log("history", window.history)
+	urlLocationHandler();
+};
+
+// create a function that handles the url location
+const urlLocationHandler = async () => {
+	const location = window.location.pathname; // get the url path
+	// if the path length is 0, set it to primary page route
+	if (location.length == 0) {
+		location = "/";
+	}
+	// get the route object from the urlRoutes object
+	const route = urlRoutes[location] || urlRoutes["404"];
+	// get the html from the template
+	const html = await fetch(route.template).then((response) => response.text());
+	// set the content of the content div to the html
+	document.getElementById("content").innerHTML = html;
+	// set the title of the document to the title of the route
+	document.title = route.title;
+};
+
+// add an event listener to the window that watches for url changes
+window.onpopstate = urlLocationHandler;
+// call the urlLocationHandler function to handle the initial url
+window.route = urlRoute;
+// call the urlLocationHandler function to handle the initial url
+urlLocationHandler();
+
+
+// http-server homepage -p 8080
